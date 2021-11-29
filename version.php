07c7fe437c238f6e9802d3c7df3cb5e0f58232be
Christian Lawson-Perfect <christianperfect@gmail.com>
2021-11-29 09:56:33 +0000

    add maturity and release to version.php

--- version.php
+++ version.php
@@ -13,3 +13,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2021112900;                 // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019111200;                 // Requires this Moodle version.
 $plugin->component = 'scormreport_numbas'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_STABLE;
+$plugin-release = 'v1.0';
